48420953f8b5ca0596b1f08c960ddfc23566a569
abdelrahmanbadr.it <abdelrahmanbadr.it@gmail.com>
2017-11-25 21:37:25 +0200

    adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 import { ShowUserComponent } from './show-user/show-user.component';
 import { AllUsersComponent } from './all-users/all-users.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { FormsModule } from '@angular/forms';
 
+const appRoutes: Routes = [
+  { path: 'Users', component: AllUsersComponent },
+  { path: 'User/:id', component: ShowUserComponent },
+  { path: 'login', component: LoginComponentComponent },
 
+];
 @NgModule({
   declarations: [
     AppComponent,
     ShowUserComponent,
-    AllUsersComponent
+    AllUsersComponent,
+    LoginComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
