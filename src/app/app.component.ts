481534855ba3f94ae08d30c2024da880ff0c62dc
abdelrahmanbadr.it <abdelrahmanbadr.it@gmail.com>
2017-11-25 21:39:22 +0200

    app component page and login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  account: {email: string, password: string} = {
+    email: '',
+    password: ''
+  };
+  token;
+  constructor(private http: HttpClient,private router: Router) {
+
+    this.token=localStorage.getItem('token');
+    if(this.token){
+      this.router.navigateByUrl('/Users');
+    }
+
+  }
   title = 'app';
+  error = 0;
+
+  dologin(){
+    //let options = new RequestOptions({ headers: headers });
+    let options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
+    this.http.post(
+        'http://localhost:8000/api/authenticate',
+        this.account,
+        options
+    ).map(
+        (res: Response) => res
+    ).subscribe(
+        (res) => {
+
+
+
+          if(!res['token']){
+            this.error =1;
+          }else{
+            localStorage.setItem('token',res['token'] );
+            this.token = res['token'];
+            this.router.navigateByUrl('/Users');
+
+          }
+        },
+        (x) => {
+          this.error =1;
+        },
+        () => {
+
+          //completed
+        }
+    );
+  }
+
+
+
+
+
 }
